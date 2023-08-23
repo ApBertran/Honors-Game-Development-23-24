db752b79290f9b6708cb208420f2a39b2550267f
Alex Bertran <ray200606@gmail.com>
2023-08-23 09:55:12 -0700

    pong-1

--- pong/main.lua
+++ pong/main.lua
@@ -1,21 +1,39 @@
 -- run 'COMMAND + L' to run the program
+-- include 'push' library
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function love.draw()
+    push:apply('start')
+
     love.graphics.printf(
         'Hello Pong!',
         0,
-        WINDOW_HEIGHT / 2 - 6,
-        WINDOW_WIDTH,
+        VIRTUAL_HEIGHT / 2 - 6,
+        VIRTUAL_WIDTH,
         'center'
     )
+
+    push:apply('end')
 end
