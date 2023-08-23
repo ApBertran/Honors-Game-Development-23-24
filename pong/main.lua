c9ef2919476d074e37f616446cc0826cb108f430
Alex Bertran <ray200606@gmail.com>
2023-08-23 10:07:10 -0700

    pong-2

--- pong/main.lua
+++ pong/main.lua
@@ -1,4 +1,5 @@
 -- run 'COMMAND + L' to run the program
+
 -- include 'push' library
 push = require 'push'
 
@@ -10,6 +11,10 @@ VIRTUAL_HEIGHT = 243
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    
+    love.graphics.setFont(smallFont)
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -27,13 +32,24 @@ end
 function love.draw()
     push:apply('start')
 
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
     love.graphics.printf(
         'Hello Pong!',
         0,
-        VIRTUAL_HEIGHT / 2 - 6,
+        20,
         VIRTUAL_WIDTH,
         'center'
     )
+
+    -- left paddle
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+    -- right paddle
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    -- ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     push:apply('end')
 end
