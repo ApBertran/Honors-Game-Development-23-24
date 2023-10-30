a00b131134e15e4e22c07cf774f14dc6cb0d0448
Alex Bertran <ray200606@gmail.com>
2023-10-30 11:41:08 -0700

    add time for matches

--- match3/match-3/src/states/PlayState.lua
+++ match3/match-3/src/states/PlayState.lua
@@ -196,6 +196,7 @@ function PlayState:calculateMatches()
         -- add score for each match
         for k, match in pairs(matches) do
             self.score = self.score + #match * 50
+            self.timer = self.timer + #match
         end
 
         -- remove any tiles that matched from the board, making empty spaces
